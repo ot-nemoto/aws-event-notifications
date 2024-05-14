0452cae3e75c69cb0925a8f83a95544bbe452048
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-05-14 00:25:31 +0000

    不要なコードを削除

--- lib/aws-event-notifications-stack.ts
+++ lib/aws-event-notifications-stack.ts
@@ -23,51 +23,6 @@ export class AwsEventNotigicationsStack extends cdk.Stack {
             slackChannelId: props.slackChannelId,
         });
 
-        // // Amazon SNS
-        // const topic = new sns.Topic(this, 'SnsTopic', {
-        //     topicName: 'aws-event-notifications',
-        //     enforceSSL: true, // AwsSolutions-SNS3
-        // });
-        // NagSuppressions.addResourceSuppressions(topic, [
-        //     {
-        //         id: 'AwsSolutions-SNS2',
-        //         reason: 'Encryption is not needed for topics which is used for triggering state machine.',
-        //     },
-        // ]);
-        //
-        // // AWS Chatbot
-        // const slack = new chatbot.SlackChannelConfiguration(this, 'ChatbotSlackChannelConfiguration', {
-        //     slackChannelConfigurationName: 'aws-event-notifications',
-        //     slackWorkspaceId: props.slackWorkspaceId,
-        //     slackChannelId: props.slackChannelId,
-        //     guardrailPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('ReadOnlyAccess')],
-        //     notificationTopics: [topic],
-        //     logRetention: cdk.aws_logs.RetentionDays.INFINITE,
-        //     loggingLevel: chatbot.LoggingLevel.INFO,
-        // });
-        // slack.addToRolePolicy(
-        //     new iam.PolicyStatement({
-        //         resources: ['*'],
-        //         actions: ['cloudwatch:Describe*', 'cloudwatch:Get*', 'cloudwatch:List*'],
-        //         effect: iam.Effect.ALLOW,
-        //     })
-        // );
-        // NagSuppressions.addResourceSuppressionsByPath(
-        //     this,
-        //     `/${this.stackName}/ChatbotSlackChannelConfiguration/ConfigurationRole/DefaultPolicy/Resource`,
-        //     [{ id: 'AwsSolutions-IAM5', reason: 'Necessary to grant Get access to all objects in the cloudwatch.' }]
-        // );
-        // NagSuppressions.addResourceSuppressionsByPath(
-        //     this,
-        //     `/${this.stackName}/LogRetentionaae0aa3c5b4d4f87b02d85b201efdd8a/ServiceRole/Resource`,
-        //     [{ id: 'AwsSolutions-IAM4', reason: 'Uncontrollable due to CDK-generated custom resource.' }]
-        // );
-        // NagSuppressions.addResourceSuppressionsByPath(
-        //     this,
-        //     `/${this.stackName}/LogRetentionaae0aa3c5b4d4f87b02d85b201efdd8a/ServiceRole/DefaultPolicy/Resource`,
-        //     [{ id: 'AwsSolutions-IAM5', reason: 'Uncontrollable due to CDK-generated custom resource.' }]
-        // );
-
         // Amazon EventBridge
         new events.Rule(this, 'AwsHealthEventsRule', {
             ruleName: 'aws-health-notification',
